8d81b237a9b7f1d5fdd5218af5e9ef6dfbc77ca5
Tetragramm <gaussgun+github@gmail.com>
2021-03-25 12:55:07 -0500

    Fix Warnings on Weapon Display.

--- PlaneBuilder/WeaponDisplay/weapon_display.js
+++ PlaneBuilder/WeaponDisplay/weapon_display.js
@@ -299,10 +299,25 @@ const loadJSON = (path, callback) => {
 };
 /// <reference path="../disp/Tools.ts" />
 const init = () => {
-    loadJSON('/PlaneBuilder/weapons.json', (weapon_resp) => {
-        var weapon_json = JSON.parse(weapon_resp);
+    const sp = new URLSearchParams(location.search);
+    var lang = sp.get("lang");
+    var jsons = ['/PlaneBuilder/strings.json', '/PlaneBuilder/weapons.json'];
+    var proms = jsons.map(d => fetch(d));
+    Promise.all(proms)
+        .then(ps => Promise.all(ps.map(p => p.json())))
+        .then(resp => {
+        var string_JSON = resp[0];
+        var weapon_JSON = resp[1];
+        //Strings bit
+        local = new Localization(string_JSON);
+        if (lang) {
+            local.SetLanguages(lang);
+        }
+        else if (window.localStorage.language) {
+            local.SetLanguages(window.localStorage.language);
+        }
         var tbl = document.getElementById("table_weap");
-        for (let weap of weapon_json["weapons"]) {
+        for (let weap of weapon_JSON["weapons"]) {
             var row = tbl.insertRow();
             CreateTD(row, weap["name"]);
             CreateTD(row, weap["abrv"]);
@@ -349,7 +364,7 @@ const init = () => {
             else
                 CreateTD(row, "No");
             if (weap["warning"])
-                CreateTD(row, weap["warning"]);
+                CreateTD(row, lu(weap["warning"]));
             else
                 CreateTD(row, "");
         }
@@ -374,3 +389,289 @@ const init = () => {
 };
 window.onload = init;
 var enable_anim = false;
+var local;
+// MIT License
+// Copyright(c) 2017 Sven Ulrich
+// https://github.com/iwt-svenulrich/typescript-string-operations
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+class StringFmt {
+    static IsNullOrWhiteSpace(value) {
+        try {
+            if (value == null || value == 'undefined') {
+                return true;
+            }
+            return value.toString().replace(/\s/g, '').length < 1;
+        }
+        catch (e) {
+            console.log(e);
+            return false;
+        }
+    }
+    static Join(delimiter, ...args) {
+        try {
+            let firstArg = args[0];
+            if (Array.isArray(firstArg) || firstArg instanceof Array) {
+                let tempString = StringFmt.Empty;
+                let count = 0;
+                for (let i = 0; i < firstArg.length; i++) {
+                    let current = firstArg[i];
+                    if (i < firstArg.length - 1) {
+                        tempString += current + delimiter;
+                    }
+                    else {
+                        tempString += current;
+                    }
+                }
+                return tempString;
+            }
+            else if (typeof firstArg === 'object') {
+                let tempString = StringFmt.Empty;
+                let objectArg = firstArg;
+                let keys = Object.keys(firstArg); //get all Properties of the Object as Array
+                keys.forEach(element => { tempString += objectArg[element] + delimiter; });
+                tempString = tempString.slice(0, tempString.length - delimiter.length); //remove last delimiter
+                return tempString;
+            }
+            let stringArray = args;
+            return StringFmt.join(delimiter, ...stringArray);
+        }
+        catch (e) {
+            console.log(e);
+            return StringFmt.Empty;
+        }
+    }
+    static Format(format, ...args) {
+        try {
+            if (format.match(StringFmt.regexNumber)) {
+                return StringFmt.format(StringFmt.regexNumber, format, args);
+            }
+            if (format.match(StringFmt.regexObject)) {
+                return StringFmt.format(StringFmt.regexObject, format, args, true);
+            }
+            return format;
+        }
+        catch (e) {
+            console.log(e);
+            return StringFmt.Empty;
+        }
+    }
+    static format(regex, format, args, parseByObject = false) {
+        return format.replace(regex, function (match, x) {
+            let s = match.split(':');
+            if (s.length > 1) {
+                x = s[0].replace('{', '');
+                match = s[1].replace('}', ''); //U
+            }
+            let arg;
+            if (parseByObject) {
+                arg = args[0][x];
+            }
+            else {
+                arg = args[x];
+            }
+            if (arg == null || arg == undefined || match.match(/{\d+}/)) {
+                return arg;
+            }
+            arg = StringFmt.parsePattern(match, arg);
+            return typeof arg != 'undefined' && arg != null ? arg : StringFmt.Empty;
+        });
+    }
+    static parsePattern(match, arg) {
+        switch (match) {
+            case 'L': {
+                arg = arg.toLocaleLowerCase();
+                return arg;
+            }
+            case 'U': {
+                arg = arg.toLocaleUpperCase();
+                return arg;
+            }
+            case 'd': {
+                if (typeof (arg) === 'string') {
+                    return StringFmt.getDisplayDateFromString(arg);
+                }
+                else if (arg instanceof Date) {
+                    return StringFmt.Format('{0:00}.{1:00}.{2:0000}', arg.getDate(), arg.getMonth(), arg.getFullYear());
+                }
+                break;
+            }
+            case 's': {
+                if (typeof (arg) === 'string') {
+                    return StringFmt.getSortableDateFromString(arg);
+                }
+                else if (arg instanceof Date) {
+                    return StringFmt.Format('{0:0000}-{1:00}-{2:00}', arg.getFullYear(), arg.getMonth(), arg.getDate());
+                }
+                break;
+            }
+            case 'n': { //Tausender Trennzeichen
+                if (typeof (arg) !== "string")
+                    arg = arg.toString();
+                let replacedString = arg.replace(/,/g, '.');
+                if (isNaN(parseFloat(replacedString)) || replacedString.length <= 3) {
+                    break;
+                }
+                let numberparts = replacedString.split(/[^0-9]+/g);
+                let parts = numberparts;
+                if (numberparts.length > 1) {
+                    parts = [StringFmt.join('', ...(numberparts.splice(0, numberparts.length - 1))), numberparts[numberparts.length - 1]];
+                }
+                let integer = parts[0];
+                var mod = integer.length % 3;
+                var output = (mod > 0 ? (integer.substring(0, mod)) : StringFmt.Empty);
+                var firstGroup = output;
+                var remainingGroups = integer.substring(mod).match(/.{3}/g);
+                output = output + '.' + StringFmt.Join('.', remainingGroups);
+                arg = output + (parts.length > 1 ? ',' + parts[1] : '');
+                return arg;
+            }
+            case 'x': {
+                return this.decimalToHexString(arg);
+            }
+            case 'X': {
+                return this.decimalToHexString(arg, true);
+            }
+            default: {
+                break;
+            }
+        }
+        if ((typeof (arg) === 'number' || !isNaN(arg)) && !isNaN(+match) && !StringFmt.IsNullOrWhiteSpace(arg)) {
+            return StringFmt.formatNumber(arg, match);
+        }
+        return arg;
+    }
+    static decimalToHexString(value, upperCase = false) {
+        const parsed = parseFloat(value);
+        const hexNumber = parsed.toString(16);
+        return upperCase ? hexNumber.toLocaleUpperCase() : hexNumber;
+    }
+    static getDisplayDateFromString(input) {
+        let splitted;
+        splitted = input.split('-');
+        if (splitted.length <= 1) {
+            return input;
+        }
+        let day = splitted[splitted.length - 1];
+        let month = splitted[splitted.length - 2];
+        let year = splitted[splitted.length - 3];
+        day = day.split('T')[0];
+        day = day.split(' ')[0];
+        return `${day}.${month}.${year}`;
+    }
+    static getSortableDateFromString(input) {
+        let splitted = input.replace(',', '').split('.');
+        if (splitted.length <= 1) {
+            return input;
+        }
+        let times = splitted[splitted.length - 1].split(' ');
+        let time = StringFmt.Empty;
+        if (times.length > 1) {
+            time = times[times.length - 1];
+        }
+        let year = splitted[splitted.length - 1].split(' ')[0];
+        let month = splitted[splitted.length - 2];
+        let day = splitted[splitted.length - 3];
+        let result = `${year}-${month}-${day}`;
+        if (!StringFmt.IsNullOrWhiteSpace(time) && time.length > 1) {
+            result += `T${time}`;
+        }
+        else {
+            result += "T00:00:00";
+        }
+        return result;
+    }
+    static formatNumber(input, formatTemplate) {
+        let count = formatTemplate.length;
+        let stringValue = input.toString();
+        if (count <= stringValue.length) {
+            return stringValue;
+        }
+        let remainingCount = count - stringValue.length;
+        remainingCount += 1; //Array must have an extra entry
+        return new Array(remainingCount).join('0') + stringValue;
+    }
+    static join(delimiter, ...args) {
+        let temp = StringFmt.Empty;
+        for (let i = 0; i < args.length; i++) {
+            if ((typeof args[i] == 'string' && StringFmt.IsNullOrWhiteSpace(args[i]))
+                || (typeof args[i] != "number" && typeof args[i] != "string")) {
+                continue;
+            }
+            let arg = "" + args[i];
+            temp += arg;
+            for (let i2 = i + 1; i2 < args.length; i2++) {
+                if (StringFmt.IsNullOrWhiteSpace(args[i2])) {
+                    continue;
+                }
+                temp += delimiter;
+                i = i2 - 1;
+                break;
+            }
+        }
+        return temp;
+    }
+}
+StringFmt.regexNumber = /{(\d+(:\w*)?)}/g;
+StringFmt.regexObject = /{(\w+(:\w*)?)}/g;
+StringFmt.Empty = '';
+class StringBuilder {
+    constructor(value) {
+        this.Values = [];
+        if (!StringFmt.IsNullOrWhiteSpace(value)) {
+            this.Values = new Array(value);
+        }
+    }
+    ToString() {
+        return this.Values.join('');
+    }
+    Append(value) {
+        this.Values.push(value);
+    }
+    AppendLine(value) {
+        this.Values.push('\r\n' + value);
+    }
+    AppendFormat(format, ...args) {
+        this.Values.push(StringFmt.Format(format, ...args));
+    }
+    AppendLineFormat(format, ...args) {
+        this.Values.push("\r\n" + StringFmt.Format(format, ...args));
+    }
+    Clear() {
+        this.Values = [];
+    }
+}
+class Localization {
+    constructor(js) {
+        this.lang = "en";
+        this.languages = js;
+    }
+    GetLanguages() {
+        var lang = [];
+        for (let elem of this.languages["languages"]) {
+            lang.push(elem);
+        }
+        return lang;
+    }
+    SetLanguages(lang) {
+        if (this.languages[lang]) {
+            this.lang = lang;
+        }
+    }
+    e(key) {
+        if (this.languages[this.lang][key]) {
+            return this.languages[this.lang][key];
+        }
+        else if (this.languages["en"][key]) {
+            return this.languages["en"][key];
+        }
+        else {
+            console.log("Failed to find " + key);
+            return "!" + key + "!";
+        }
+    }
+}
+function lu(s, ...args) {
+    return StringFmt.Format(local.e(s), ...args);
+}
--- PlaneBuilder/WeaponDisplay/weapon_display.ts
+++ PlaneBuilder/WeaponDisplay/weapon_display.ts
@@ -1,82 +1,99 @@
 /// <reference path="../disp/Tools.ts" />
 
 const init = () => {
+    const sp = new URLSearchParams(location.search);
+    var lang = sp.get("lang");
 
-    loadJSON('/PlaneBuilder/weapons.json', (weapon_resp) => {
-        var weapon_json = JSON.parse(weapon_resp);
+    var jsons = ['/PlaneBuilder/strings.json', '/PlaneBuilder/weapons.json'];
+    var proms = jsons.map(d => fetch(d));
+    Promise.all(proms)
+        .then(ps => Promise.all(ps.map(p => p.json())))
+        .then(
+            resp => {
+                var string_JSON = resp[0];
+                var weapon_JSON = resp[1];
 
-        var tbl = document.getElementById("table_weap") as HTMLTableElement;
-        for (let weap of weapon_json["weapons"]) {
-            var row = tbl.insertRow();
-            CreateTD(row, weap["name"]);
-            CreateTD(row, weap["abrv"]);
-            CreateTD(row, weap["era"]);
-            CreateTD(row, weap["cost"]);
-            CreateTD(row, weap["mass"]);
-            CreateTD(row, weap["drag"]);
-            CreateTD(row, weap["hits"]);
-            CreateTD(row, weap["damage"]);
-            CreateTD(row, weap["ap"]);
-            CreateTD(row, weap["ammo"]);
-            CreateTD(row, weap["reload"]);
-            CreateTD(row, weap["jam"]);
-            switch (weap["size"] as number) {
-                case 1:
-                    CreateTD(row, "Tiny");
-                    break;
-                case 2:
-                    CreateTD(row, "Light");
-                    break;
-                case 4:
-                    CreateTD(row, "Medium");
-                    break;
-                case 8:
-                    CreateTD(row, "Heavy");
-                    break;
-                case 16:
-                    CreateTD(row, "Artillery");
-                    break;
-            }
-            var tags = "";
-            if (weap["rapid"] as boolean)
-                tags += "Rapid-Fire ";
-            if (weap["manual"] as boolean)
-                tags += "Manual ";
-            if (weap["shells"] as boolean)
-                tags += "Shell-Firing ";
-            if (!(weap["synched"] as boolean))
-                tags += "Open-Bolt ";
-            CreateTD(row, tags)
-            var deflection = (weap["deflection"] as number);
-            if (deflection < 0)
-                CreateTD(row, "Yes, " + deflection);
-            else
-                CreateTD(row, "No");
+                //Strings bit
+                local = new Localization(string_JSON);
+                if (lang) {
+                    local.SetLanguages(lang);
+                } else if (window.localStorage.language) {
+                    local.SetLanguages(window.localStorage.language);
+                }
+                var tbl = document.getElementById("table_weap") as HTMLTableElement;
+                for (let weap of weapon_JSON["weapons"]) {
+                    var row = tbl.insertRow();
+                    CreateTD(row, weap["name"]);
+                    CreateTD(row, weap["abrv"]);
+                    CreateTD(row, weap["era"]);
+                    CreateTD(row, weap["cost"]);
+                    CreateTD(row, weap["mass"]);
+                    CreateTD(row, weap["drag"]);
+                    CreateTD(row, weap["hits"]);
+                    CreateTD(row, weap["damage"]);
+                    CreateTD(row, weap["ap"]);
+                    CreateTD(row, weap["ammo"]);
+                    CreateTD(row, weap["reload"]);
+                    CreateTD(row, weap["jam"]);
+                    switch (weap["size"] as number) {
+                        case 1:
+                            CreateTD(row, "Tiny");
+                            break;
+                        case 2:
+                            CreateTD(row, "Light");
+                            break;
+                        case 4:
+                            CreateTD(row, "Medium");
+                            break;
+                        case 8:
+                            CreateTD(row, "Heavy");
+                            break;
+                        case 16:
+                            CreateTD(row, "Artillery");
+                            break;
+                    }
+                    var tags = "";
+                    if (weap["rapid"] as boolean)
+                        tags += "Rapid-Fire ";
+                    if (weap["manual"] as boolean)
+                        tags += "Manual ";
+                    if (weap["shells"] as boolean)
+                        tags += "Shell-Firing ";
+                    if (!(weap["synched"] as boolean))
+                        tags += "Open-Bolt ";
+                    CreateTD(row, tags)
+                    var deflection = (weap["deflection"] as number);
+                    if (deflection < 0)
+                        CreateTD(row, "Yes, " + deflection);
+                    else
+                        CreateTD(row, "No");
 
-            if (weap["warning"])
-                CreateTD(row, weap["warning"]);
-            else
-                CreateTD(row, "");
-        }
+                    if (weap["warning"])
+                        CreateTD(row, lu(weap["warning"]));
+                    else
+                        CreateTD(row, "");
+                }
 
-        var last_row = tbl.insertRow();
-        CreateTH(last_row, "Name");
-        CreateTH(last_row, "Abbreviation");
-        CreateTH(last_row, "Era");
-        CreateTH(last_row, "Cost");
-        CreateTH(last_row, "Mass");
-        CreateTH(last_row, "Drag");
-        CreateTH(last_row, "Hits");
-        CreateTH(last_row, "Damage");
-        CreateTH(last_row, "AP");
-        CreateTH(last_row, "Ammo");
-        CreateTH(last_row, "Reload");
-        CreateTH(last_row, "Jam");
-        CreateTH(last_row, "Size");
-        CreateTH(last_row, "Tags");
-        CreateTH(last_row, "Awkward");
-        CreateTH(last_row, "Notes");
-    });
+                var last_row = tbl.insertRow();
+                CreateTH(last_row, "Name");
+                CreateTH(last_row, "Abbreviation");
+                CreateTH(last_row, "Era");
+                CreateTH(last_row, "Cost");
+                CreateTH(last_row, "Mass");
+                CreateTH(last_row, "Drag");
+                CreateTH(last_row, "Hits");
+                CreateTH(last_row, "Damage");
+                CreateTH(last_row, "AP");
+                CreateTH(last_row, "Ammo");
+                CreateTH(last_row, "Reload");
+                CreateTH(last_row, "Jam");
+                CreateTH(last_row, "Size");
+                CreateTH(last_row, "Tags");
+                CreateTH(last_row, "Awkward");
+                CreateTH(last_row, "Notes");
+            });
 }
 window.onload = init;
 var enable_anim = false;
+
+var local: Localization;
