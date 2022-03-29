93b433dd0a023ca8d54f2797849e6f135ec28f71
Tetragramm <gaussgun+github@gmail.com>
2022-03-28 19:30:55 -0500

    Test pre-commit hook to minimize js.

--- Helicopter/redirect.js
+++
@@ -1,5 +0,0 @@
-const init = () => {
-    window.history.replaceState(null, null, "/PlaneBuilder/engine.html" + location.search);
-    window.history.go();
-};
-window.addEventListener("DOMContentLoaded", init);
