3c3a6b4474537a24e9786efbb60fc3ef06a44cd7
Tetragramm <gaussgun+github@gmail.com>
2022-07-02 18:37:43 -0500

    Make Pneumatic and Mechanical Action weapons mutually exclusive.

--- PlaneBuilder/src/impl/WeaponSystem.ts
+++ PlaneBuilder/src/impl/WeaponSystem.ts
@@ -301,7 +301,7 @@ export class WeaponSystem extends Part {
             this.projectile_sel = ProjectileType.BULLETS;
         }
 
-        if ((this.action_sel == ActionType.GAST || this.action_sel == ActionType.ROTARY)
+        if ((this.action_sel == ActionType.GAST || this.action_sel == ActionType.ROTARY || this.action_sel == ActionType.MECHANICAL)
             && this.projectile_sel == ProjectileType.PNEUMATIC) {
             this.projectile_sel = ProjectileType.BULLETS;
         }
@@ -715,7 +715,7 @@ export class WeaponSystem extends Part {
         return [true,
             this.final_weapon.can_projectile && this.action_sel != ActionType.MECHANICAL && this.action_sel != ActionType.GAST && this.weapon_list[this.weapon_type].hits > 0,
             // this.final_weapon.can_projectile && this.weapon_list[this.weapon_type].hits > 0,
-            this.final_weapon.can_projectile && this.action_sel != ActionType.ROTARY && this.action_sel != ActionType.GAST];
+            this.final_weapon.can_projectile && this.action_sel != ActionType.ROTARY && this.action_sel != ActionType.GAST && this.action_sel != ActionType.MECHANICAL];
     }
 
     public GetProjectile() {
